46ab1d02908e3d07f6b613cbbc3d010ff4faccab
Homer Lin <125667301+Homer-Lin@users.noreply.github.com>
2023-02-25 02:16:51 +0800

    Add files via upload

---
+++ week2_homework10.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class week2_homework10
+{
+	public static void main(String[] argv)
+	{
+		System.out.println("Please input the temperature in ℃");
+		Scanner tem = new Scanner(System.in);
+		float temper = tem.nextFloat();
+		System.out.println("Now the temperature is " + (temper * 9 / 5 + 32) + "℉" );
+	}
+}
